40133c058dbdc72c1dadd938e30de638ee07481d
vishnu priyan <54699197+vishnu2646@users.noreply.github.com>
2021-06-27 11:54:12 +0530

    Update headercontent.component.ts

--- src/app/components/headercontent/headercontent.component.ts
+++ src/app/components/headercontent/headercontent.component.ts
@@ -37,6 +37,14 @@ export class HeadercontentComponent implements OnInit {
       .filter(item => Object.keys(item)
         .some(key => value.split(',').some(arg => item[key].toString().toLowerCase().includes(arg.toLowerCase())))
       );
-   this.students = this.filteredStudents;   
+   this.students = this.filteredStudents;  
+    if(value=='')
+   {
+    this._apiService.getComments().subscribe(
+      data=>{
+        this.students = data;
+      }
+    );
+   }
   }
 }
